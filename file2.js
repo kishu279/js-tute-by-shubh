d322e4fac890e7b3d12200fbbb8ebd35a2ec96ab
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-06-21 19:49:48 +0530

    constants

--- file2.js
+++ file2.js
@@ -4,8 +4,10 @@ var firstName = "Subh";
 // firstName = "Neha";
 let secondName = "Vidya";
 // secondName = "Neha2";
+
+// const means constant , it is something which cannot be changed 
 const thirdName = "Ujjwal";
-thirdName = "Naina sweetheart";
+// thirdName = "Naina sweetheart";
 
 console.log(firstName);
 console.log(secondName);
