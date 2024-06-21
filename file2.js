86679ab1cd9b5a6e13da82b085f3b0c8989d4d9f
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-06-21 19:29:26 +0530

    studied about variables:

---
+++ file2.js
@@ -0,0 +1,12 @@
+// Intro to variables
+
+var firstName = "Subh";
+// firstName = "Neha";
+let secondName = "Vidya";
+// secondName = "Neha2";
+const thirdName = "Ujjwal";
+thirdName = "Naina sweetheart";
+
+console.log(firstName);
+console.log(secondName);
+console.log(thirdName);
