4991505f95447a717e1c6e80d90adc3b724a9755
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-06-22 19:36:53 +0530

    for of loop

---
+++ file29.js
@@ -0,0 +1,10 @@
+// for of loop 
+
+const fruits = ["apple","grapes","banana","avacardo"];
+const fruits2 = [];
+
+for ( let fruit of fruits){
+    fruits2.push(fruit.toUpperCase());
+}
+
+console.log(fruits2);
