cdbe4353d020f8c670ad0e257d53070858e5d7f6
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-06-22 18:16:04 +0530

    break and continue keyword

---
+++ file21.js
@@ -0,0 +1,19 @@
+// break and continue keywords 
+
+// for( let i = 0 ; i<=10 ; i++){
+//     if(i===4){
+//         break;
+//     }
+//     console.log(i);
+// }
+// console.log("Hello i am back");
+
+// break will finish the loop when the condition is met and come out of the loop
+
+for( let i = 0 ; i<=10 ; i++){
+    if(i===4){
+        continue;
+    }
+    console.log(i);
+}
+console.log("Hello i am back");
