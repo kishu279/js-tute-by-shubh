cceeb599492308ebd1beabaf2b5570145ee48c74
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-06-22 18:22:32 +0530

    do while loop

---
+++ file22.js
@@ -0,0 +1,10 @@
+// do while loop 
+
+let i = 0 ;
+
+do{
+    console.log(i);
+    i++;
+} while (i<=9);
+
+// if the first condition is false then it will print the else statement xoz its doing its tasks before checking the given statement 
