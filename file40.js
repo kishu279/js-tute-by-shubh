d9580f3e0a348cb2e50f4763c8ccebb97a7e6d2b
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-06-24 18:48:50 +0530

    function expression

---
+++ file40.js
@@ -0,0 +1,9 @@
+// 2nd way 
+// function expression 
+// when you declare a function inside a variable it is known as function expression
+
+const singHappyBirthday = function (){
+    console.log("happy birthday to you ");
+}
+
+console.log(singHappyBirthday());
