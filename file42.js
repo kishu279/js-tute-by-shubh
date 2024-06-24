25406990b4b00ce485df002d370c923a590ddfda
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-06-24 19:05:19 +0530

    hoisting

---
+++ file42.js
@@ -0,0 +1,11 @@
+// hoisting 
+
+
+hello();
+
+function hello(){
+    console.log("Hello");
+}
+
+// in case of function declaration  we can call the function before creating them but we can do the same in the case of function expression 
+
