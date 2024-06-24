0d2cc869a3bc6c0a341c88f3498da8491234615d
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-06-24 19:21:02 +0530

    lexical scope

--- file44.js
+++ file44.js
@@ -3,7 +3,8 @@
 function app(){
     const myvar = "value1";
     function myFunc(){
-        // const myvar = "value69";
+        const myvar = "value69";
+        // if we comment the above line we will get the value from outside the function 
         console.log("inside myFunc", myvar);
     }
     console.log(myvar);
