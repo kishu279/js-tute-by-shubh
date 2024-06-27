bde21d258647d73bcfc2694c7c4317a1c8aabe41
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-06-27 19:23:16 +0530

    splice method

---
+++ file60.js
@@ -0,0 +1,18 @@
+// splice method
+// start delete and insert 
+// this method changes the old array 
+
+const myArray = ['item1','item2','item3'];
+
+// delete 
+myArray.splice(1,1);
+// this returns too we can store this in a variable
+console.log(myArray);
+
+myArray.splice(1,0,'inserted item');
+console.log(myArray);
+
+// how to insert and delete together 
+const deletedItem = myArray.splice(1,2,"inserted item 1" , 'inserted Item 2 ');
+console.log(myArray);
+console.log(deletedItem);
