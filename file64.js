1c0e2f6a5eee7a1565dadf05d058028938a7529e
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-06-30 01:55:14 +0530

    Object.assign()

---
+++ file64.js
@@ -0,0 +1,12 @@
+// Clone using Object.assign
+
+const obj = {
+  key1: "value1",
+  key2: "value2",
+};
+
+// const obj2 = {...obj};
+const obj2 = Object.assign({}, obj); // this will create a new memory address
+obj.key3 = "value3";
+console.log(obj);
+console.log(obj2);
