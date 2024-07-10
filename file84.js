971b1f39ac09f481054c35592c952360c8485051
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-07-11 01:31:59 +0530

    hoisiting in case of let and const

---
+++ file84.js
@@ -0,0 +1,9 @@
+// hoisting in case of let and const
+
+// we will be getting a error
+// ReferenceError: Cannot access 'fullName' before initialization
+
+console.log(fullName);
+//ReferenceError: fullName is not defined
+// let fullName = "Prachi";
+// console.log(fullName);
