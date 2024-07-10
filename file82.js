fe50c1f9a69fb1b19303b637ebe0a97d1c134650
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-07-11 00:30:52 +0530

    how js works

---
+++ file82.js
@@ -0,0 +1,27 @@
+// compilation
+
+// code execution
+
+// why compilation
+
+// how javascript works ?
+
+/// what is global execution context ?
+
+// what is local execution context
+
+// closures
+
+console.log(this);
+console.log(window);
+console.log(myfunc);
+console.log(fullName);
+
+function myfunc() {
+  console.log("This is my function");
+}
+
+var firstName = "Neha";
+var lastName = "sharma";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
