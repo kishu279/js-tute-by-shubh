be1d05c18d4a2aed4dd1833d92b6ae88ac6595c8
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-07-12 00:18:02 +0530

    document object

---
+++ DOM/file90.js
@@ -0,0 +1,26 @@
+// we can use alert to pop message to users
+
+// alert("hey you should not be here!");
+
+// const value = prompt("whats you name?", "Neha"); // to take input from the user
+// console.log(value);
+
+// browser is also known as object
+// window.console.log("I am the best");
+
+// everything is inside the window
+
+// document object
+// where does it exists ?
+// document exists inside the window
+// console.log(document.body);
+
+// document.title = "Subh";
+
+// document.write("This is hacked");
+// js can manipulate your HTML
+
+// we will extensively use this
+
+const username = prompt("whats your name? ");
+document.write("Hello " + username);
