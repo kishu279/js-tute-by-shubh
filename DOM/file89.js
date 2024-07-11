7e78ee28b4952a05849c8b9f6b77d83e1650c3d0
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-07-11 23:55:44 +0530

    dev tools

---
+++ DOM/file89.js
@@ -0,0 +1,13 @@
+/// what are developer tools // how do we use it ??
+
+// what is DOM ?
+//  full form --> document object model
+// your HTML is dom
+
+// element --> to control and check ur HTML and CSS
+// console --> any valid js
+
+// sources --> related files to the project
+// network --> for checking outgoing and ingoing request
+// Application --> to check local storage
+// lighthouse --> to check performance
